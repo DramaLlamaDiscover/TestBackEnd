8d9dcf320047a2f3fee734adfb5bede58e4e8f05
Ryan Llamas <rllamas@discover.com>
2019-10-13 16:33:37 -0500

    Set for Post an CORS works successfully
    CORS policy is handled with a npm install on cors allowing intergration on the app for cross orgin sharing of information on urls. Also handled a post request for text of apples

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/apples', function(req, res) {
+  console.log("Attempting to access apples");
+  console.log("The amount of apples you want is " + req.body.apples);
+  var applesText = "";
+  for(var i = 0; i < req.body.apples; i++)
+  {
+    applesText = applesText + "apple ";
+  }
+  res.send(applesText);
+});
+
 module.exports = router;
